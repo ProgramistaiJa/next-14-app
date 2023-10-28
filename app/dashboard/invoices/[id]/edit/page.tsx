5b540a03f10fe8c80b2016af836fa849e695b041
Artur Wojciechowski <programista@i-ja.pl>
2023-10-28 02:57:40 +0200

    Feat/serching (#7)
    * feat: added searching functionality
    
    * feat: added pagination
    
    * feat: added edit funcionality
    
    * fix: deleted timeout
    
    * feat: errors handling

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,11 +1,14 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
  
 export default async function Page({ params }: { params: { id: string } }) {
-    const id = params.id;
-    const invoice = await fetchInvoiceById(id);
-    const customers = await fetchCustomers();
+  const id = params.id;
+  const invoice = await fetchInvoiceById(id);
+  const customers = await fetchCustomers();
+  
+  if (!invoice) return notFound();
 
   return (
     <main>
