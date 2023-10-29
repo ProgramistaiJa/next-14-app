5150978a3d3d3e59e445c9bf6247a9689288fd8a
Artur Wojciechowski <programista@i-ja.pl>
2023-10-29 19:25:00 +0100

    Fix/autorization (#17)
    * fix: logout
    
    * feat: styled  nav links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
-import Link from 'next/link';
+import { HeadNav } from './ui/nav/head/head-nav';
+import { Logout } from './ui/auth/logout';
+import { LoginLink, Version } from './ui/auth/login-link';
 
 export default function RootLayout({
   children,
@@ -11,8 +13,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         <div className="flex min-h-screen flex-col p-6">
-          <div className="flex justify-between items-center h-10 shrink-0 rounded-lg bg-blue-500 p-4 md:h-10">
-            <nav className='text-white'><Link href={'/'}>Home</Link> | <Link href={'/dashboard'}>Dashboard</Link> | <Link href={'/blog'}>Blog</Link></nav>
+          <div className="flex justify-between items-center h-10 shrink-0 rounded-lg bg-blue-500 text-white p-4 md:h-10">
+            <HeadNav />
+            <LoginLink version={Version.HEADER} />
+            <Logout />
           </div>
           {children}
         </div>
