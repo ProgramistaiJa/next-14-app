eb7de45dcd296e09f0bae80c491147787ff375d8
Artur Wojciechowski <programista@i-ja.pl>
2023-10-29 14:54:15 +0100

    fix: server

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
-import AcmeLogo from './ui/acme-logo';
 import Link from 'next/link';
 
 export default function RootLayout({
@@ -12,8 +11,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         <div className="flex min-h-screen flex-col p-6">
-          <div className="flex justify-between h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-            <AcmeLogo />
+          <div className="flex justify-between items-center h-10 shrink-0 rounded-lg bg-blue-500 p-4 md:h-10">
             <nav className='text-white'><Link href={'/'}>Home</Link> | <Link href={'/dashboard'}>Dashboard</Link> | <Link href={'/blog'}>Blog</Link></nav>
           </div>
           {children}
