e362029eac052a975f26f63c88855e3d6b7dd5b0
Artur Wojciechowski <programista@i-ja.pl>
2023-10-29 15:55:03 +0100

    fix: logout (#16)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,5 @@
 import AcmeLogo from '@/app/ui/acme-logo';
-import LoginForm from '../ui/login-form';
+import LoginForm from '../ui/auth/login-form';
  
 export default function LoginPage() {
   return (
