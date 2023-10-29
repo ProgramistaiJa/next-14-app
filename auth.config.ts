36dcd54dedd9713a8066d8789d5188f4bd5fd2e9
Artur Wojciechowski <programista@i-ja.pl>
2023-10-29 23:18:09 +0100

    fix: use auth

--- auth.config.ts
+++ auth.config.ts
@@ -7,15 +7,19 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
+      console.log('nextUrl', nextUrl);
+      console.log('nextUrl.href', nextUrl.href);
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
-      }
-      return true;
+      if (isLoggedIn){ return true }
+      return false
+      // const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      // if (isOnDashboard) {
+      //   if (isLoggedIn) return true;
+      //   return false; // Redirect unauthenticated users to login page
+      // } else if (isLoggedIn) {
+      //   return Response.redirect(new URL('/dashboard', nextUrl));
+      // }
+      // return true;
     },
   },
 } satisfies NextAuthConfig;
