11be25a11c74e91b443d08bcfdff647d821aa0e1
Artur Wojciechowski <programista@i-ja.pl>
2023-10-29 13:07:56 +0100

    feat: simple nav

---
+++ app/blog/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default async function Blog() {
+  return <main>Blog </main>
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import AcmeLogo from './ui/acme-logo';
+import Link from 'next/link';
 
 export default function RootLayout({
   children,
@@ -8,7 +10,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <div className="flex min-h-screen flex-col p-6">
+          <div className="flex justify-between h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+            <AcmeLogo />
+            <nav className='text-white'><Link href={'/'}>Home</Link> | <Link href={'/dashboard'}>Dashboard</Link> | <Link href={'/blog'}>Blog</Link></nav>
+          </div>
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
