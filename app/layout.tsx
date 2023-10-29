7b9d98d19ec014717a946f266d72f871e656977c
Artur Wojciechowski <programista@i-ja.pl>
2023-10-29 22:41:20 +0100

    fix: use auth (#19)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,20 +3,22 @@ import { inter } from '@/app/ui/fonts';
 import { HeadNav } from './ui/nav/head/head-nav';
 import { Logout } from './ui/auth/logout';
 import { LoginLink, Version } from './ui/auth/login-link';
+import { auth } from '@/auth';
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth()
+
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         <div className="flex min-h-screen flex-col p-6">
           <div className="flex justify-between items-center h-10 shrink-0 rounded-lg bg-blue-500 text-white p-4 md:h-10">
             <HeadNav />
-            <LoginLink version={Version.HEADER} />
-            <Logout />
+            {!session ?<LoginLink version={Version.HEADER} /> : <div className='flex justify-end items-center'><div>{session.user?.name}</div><Logout /></div>}
           </div>
           {children}
         </div>
