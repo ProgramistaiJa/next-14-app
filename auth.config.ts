344effc292727ad15dbdc15133609b5c4ba01eec
Artur Wojciechowski <programista@i-ja.pl>
2023-10-31 18:29:14 +0100

    Fix/authorization (#21)
    * fix: use auth
    
    * fix: use auth
    
    * fix: use auth
    
    * fix: matcher
    
    * fix: matcher
    
    * fix: build
    
    * fix: build
    
    * fix: build

--- auth.config.ts
+++ auth.config.ts
@@ -6,20 +6,17 @@ export const authConfig = {
     signIn: '/login',
   },
   callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      // console.log('nextUrl', nextUrl);
-      // console.log('nextUrl.href', nextUrl.href);
+    authorized({ auth, request: { nextUrl } }){
       const isLoggedIn = !!auth?.user;
-      if (isLoggedIn){ return true }
+
+      if (isLoggedIn){ return true };
       return false
-      // const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      // if (isOnDashboard) {
-      //   if (isLoggedIn) return true;
-      //   return false; // Redirect unauthenticated users to login page
-      // } else if (isLoggedIn) {
-      //   return Response.redirect(new URL('/dashboard', nextUrl));
-      // }
-      // return true;
-    },
+
+      // if (isLoggedIn) {
+        //   return Response.redirect(new URL('/', nextUrl));
+        // }
+        // return true;
+        
+      },
   },
 } satisfies NextAuthConfig;
