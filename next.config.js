565823d5d04db7012893374a5d0bb74b74b0e2f6
Artur Wojciechowski <programista@i-ja.pl>
2023-10-31 22:34:40 +0100

    Revert "Feat/mdx blog (#22)" (#23)
    This reverts commit eaf2f6f290f8bb1813cbece6bef80c41ae4aa44e.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
-// const withMDX = require('@next/mdx')()
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-};
+const nextConfig = {};
 
-// module.exports = withMDX(nextConfig)
-module.exports = nextConfig
+module.exports = nextConfig;
