510631b7e76202ab02aa5733e614c57ed2c22ea4
Artur Wojciechowski <programista@i-ja.pl>
2023-10-31 18:27:37 +0100

    fix: build

--- auth.config.ts
+++ auth.config.ts
@@ -16,6 +16,7 @@ export const authConfig = {
         //   return Response.redirect(new URL('/', nextUrl));
         // }
         // return true;
+        
       },
   },
 } satisfies NextAuthConfig;
