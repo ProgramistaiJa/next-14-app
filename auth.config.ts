7ec4f072565655d3a7490ecbc7743ae0d98bb7a5
Artur Wojciechowski <programista@i-ja.pl>
2023-10-31 18:18:26 +0100

    fix: matcher

--- auth.config.ts
+++ auth.config.ts
@@ -7,17 +7,14 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      // console.log('nextUrl', nextUrl);
-      // console.log('nextUrl.href', nextUrl.href);
       const isLoggedIn = !!auth?.user;
+
       if (isLoggedIn){ return true }
       return false
-      // const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      // if (isOnDashboard) {
-      //   if (isLoggedIn) return true;
-      //   return false; // Redirect unauthenticated users to login page
-      // } else if (isLoggedIn) {
-      //   return Response.redirect(new URL('/dashboard', nextUrl));
+
+
+      // if (isLoggedIn) {
+      //   return Response.redirect(new URL('/', nextUrl));
       // }
       // return true;
     },
