858bf87e4ada7d4c0b01fd2aa1addf439febcf28
Artur Wojciechowski <programista@i-ja.pl>
2023-11-01 16:05:01 +0100

    fix: cleaning

--- app/ui/auth/login-link.tsx
+++ app/ui/auth/login-link.tsx
@@ -7,15 +7,7 @@ export enum Version {
     SECONDARY = 'secondary',
     HEADER = 'header'
 }
-                {/**
-                
-                
-                
-                
-                transition-colors 
-                
-                
-                */}
+
 export const LoginLink = ({version}:{version: string}) => {
   return (
     <Link
@@ -25,12 +17,6 @@ export const LoginLink = ({version}:{version: string}) => {
                 "bg-blue-500 text-white self-start md:text-base rounded-lg px-6 py-3 transition-colors hover:bg-blue-400 ": version === Version.PRIMERY, 
                 "bg-white text-blue-500 self-start md:text-base rounded-lg px-6 py-3 transition-colors  hover:bg-sky-100 hover:text-blue-600": version === Version.SECONDARY,
                 "h-[48px] gap-2 justify-center rounded-md p-3 text-sm  md:flex-none md:justify-start md:p-2 md:px-3 text-white hover:bg-sky-100 hover:text-blue-600": version === Version.HEADER,
-                /*  
-                
-                
-                
-                
-                */
             }
         )}
     >
