07503a29bde28cf4b512649b95d2a0c2e8335744
Artur Wojciechowski <programista@i-ja.pl>
2023-11-09 01:34:01 +0100

    added mdx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,15 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export default async function Blog() {
-  return <main>Blog </main>
+  return (
+    <main>
+      <h1>Blog</h1>
+      <div>
+        <li>
+          <Link href={"/blog/posts"}>Artykuł</Link>
+        </li>
+      </div>
+    </main>
+  );
 }
